cbb928c767bca1c1fa5de1b7ae7814d7c1d3ce28
João Victor <freitasjoaovictor49@gmail.com>
2025-02-09 21:41:49 -0300

    feat: adding checkpoints animated sprite with respawn logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,8 +9,6 @@ public class PlayerMovement : MonoBehaviour
     private BoxCollider2D boxCollider;
 
     private Vector3 respawnPoint;
-    private float lastGroundedX; // Track last grounded X position
-    private float respawnOffset = 5f; // Distance before respawning
     public GameObject fallDetector;
 
     private void Awake()
@@ -41,35 +39,27 @@ public class PlayerMovement : MonoBehaviour
             Jump();
         }
 
-        // If grounded, update last grounded position
-        if (IsGrounded())
-        {
-            lastGroundedX = transform.position.x;
-            respawnPoint = GetSafeRespawnPoint(lastGroundedX - respawnOffset);
-        }
-
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", IsGrounded());
 
         fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
     }
 
-    private void Jump()
-    {
+    private void Jump() {
         body.velocity = new Vector2(body.velocity.x, speed);
         anim.SetTrigger("jump");
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "FallDetector")
-        {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("FallDetector")) {
             transform.position = respawnPoint;
+        }
+        else if (collision.CompareTag("Checkpoint")) {
+            respawnPoint = collision.transform.position;
         }
     }
 
-    private bool IsGroundedAtPosition(Vector3 position)
-    {
+    private bool IsGroundedAtPosition(Vector3 position) {
         RaycastHit2D raycastHit = Physics2D.BoxCast(
             new Vector2(position.x, boxCollider.bounds.center.y),
             boxCollider.bounds.size * 0.9f,
@@ -82,26 +72,7 @@ public class PlayerMovement : MonoBehaviour
         return raycastHit.collider != null;
     }
 
-    private Vector3 GetSafeRespawnPoint(float startX)
-    {
-        float searchDistance = 3f; // Distance to check backward
-        float step = 0.5f; // Step size for checking
-
-        for (float x = startX; x > startX - searchDistance; x -= step)
-        {
-            Vector3 testPosition = new Vector3(x, transform.position.y, transform.position.z);
-            if (IsGroundedAtPosition(testPosition))
-            {
-                return testPosition;
-            }
-        }
-
-        // If no ground is found, keep the last grounded position
-        return new Vector3(lastGroundedX, transform.position.y, transform.position.z);
-    }
-
-    private bool IsGrounded()
-    {
+    private bool IsGrounded() {
         return IsGroundedAtPosition(transform.position);
     }
 }
